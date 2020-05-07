f34d9e7c3ef35ae377dc6e007fdbf750f28aee72
CDW4 <craigdw4@yahoo.com>
2020-05-07 12:41:37 -0400

    Updates

---
+++ precache-manifest.a110bc7b6498dc765bb604b57a45f108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58599027d3cf218c05fbc72ce027752c",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2cc025f6bc3f4dd58ce5",
+    "url": "/react-monsters-rolodex/static/css/main.a1cbab2d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc025f6bc3f4dd58ce5",
+    "url": "/react-monsters-rolodex/static/js/main.3b59bd0d.chunk.js"
+  },
+  {
+    "revision": "84d462a1ad20fdb403f4",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.6eda5f3f.js"
+  }
+]);
